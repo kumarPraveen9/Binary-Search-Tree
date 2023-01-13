6661caaab0194de27f24e33f34f73a1097b9873f
kumarPraveen9 <120436370+kumarPraveen9@users.noreply.github.com>
2023-01-13 13:35:14 +0530

    Main class to call operations

---
+++ Program.cs
@@ -0,0 +1,29 @@
+namespace BinarySearchTree
+{
+    class Program
+    {
+       
+            
+        public static void Main(string[] args)
+        {
+            Binarytree bn = new Binarytree();
+            bn.Add(56);
+            bn.Add(30);
+            bn.Add(70);
+           
+            Console.WriteLine();
+            Console.WriteLine("the preorder is: ");
+            bn.TravesrePreorder(bn.root);
+            Console.WriteLine();
+            Console.WriteLine("the postorder is: ");
+            bn.TravesrePostorder(bn.root);
+            Console.WriteLine();
+            Console.WriteLine("the Inorder is: ");
+            bn.TravesreInorder(bn.root);
+            Console.WriteLine();
+
+
+            
+        }
+    }
+}
